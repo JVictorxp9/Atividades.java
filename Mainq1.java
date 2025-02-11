f6be0a5864c854b0b84e8606a34e89ad77fabb60
João Victor <129428389+JVictorxp9@users.noreply.github.com>
2025-02-11 00:57:19 -0300

    Create Mainq1.java
    calculo de salario hora x.

---
+++ Mainq1.java
@@ -0,0 +1,12 @@
+public class Mainq1 {
+    public static void main(String[] args){
+
+        int valor = 15;
+        int horas = 40;
+        int total;
+
+        total = valor * horas;
+
+            System.out.println("O valor total do salario por semana é " + total);
+        }
+    }
