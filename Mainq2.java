2c792b5a4f49ce480b826c26c5876c27630ffdc1
João Victor <129428389+JVictorxp9@users.noreply.github.com>
2025-02-11 00:58:36 -0300

    Create Mainq2.java
    Verificação de variáveis

---
+++ Mainq2.java
@@ -0,0 +1,15 @@
+public class Mainq2 {
+    public static void main(String[] args){
+
+        int num1 = 10;
+        int num2 = 12;
+
+        if (num1 > num2) {
+            System.out.println("é maior");
+        } else if (num1 == num2){
+            System.out.println("é igual");
+        } else{
+            System.out.println("é menor");
+        }
+    }
+}
