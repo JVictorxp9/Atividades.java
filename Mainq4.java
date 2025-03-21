1bf5ae25a59ca011ebdeeeea2fe62925749edef9
João Victor <129428389+JVictorxp9@users.noreply.github.com>
2025-03-21 01:27:24 -0300

    Create Mainq4.java
    O seguinte código recebe do usuário o valor do salário e, com base nesse valor, realiza o cálculo do desconto de acordo com o salário informado.

---
+++ Mainq4.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Mainq4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o salário: ");
+        double salario = sc.nextDouble();
+
+        double desconto = 0.0;
+
+        if (salario <= 900.00) {
+            System.out.println("Isento de IR");
+
+        }
+        else if (salario <= 1500.00) {
+            desconto = salario * 0.05;
+            System.out.println("Desconto de 5%");
+            System.out.println("Desconto de 5%: R$ " + desconto);
+        }
+        else if (salario <= 2500.00) {
+            desconto = salario * 0.10;
+            System.out.println("Desconto de 10%");
+            System.out.println("Desconto de 10%: R$ " + desconto);
+        }
+        else {
+            desconto = salario * 0.20;
+            System.out.println("Desconto de 20%: R$ " + desconto);
+    }
+        double salarioComDesconto = salario - desconto;
+        System.out.println("Salário líquido após desconto é de: R$ " + salarioComDesconto);
+
+        sc.close();
+ }
+}
